0cd7db7a04abbd095db91377f879250d7c205800
abhimaurya <arm20493@gmail.com>
2019-12-22 10:17:01 +0530

    Updates

---
+++ precache-manifest.69afc0bf5e2b97cadb4120c1154d67c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c808caf6566707d573622206f3858bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1fda67bdfe2c851cd65",
+    "url": "/static/css/main.77718c8f.chunk.css"
+  },
+  {
+    "revision": "bab663dbe233df65d422",
+    "url": "/static/js/2.40efff77.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.40efff77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1fda67bdfe2c851cd65",
+    "url": "/static/js/main.eda34dc2.chunk.js"
+  },
+  {
+    "revision": "3eb3e9a6f0a97fb2528b",
+    "url": "/static/js/runtime-main.91aea62a.js"
+  }
+]);
